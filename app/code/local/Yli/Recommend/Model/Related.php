8c04a5d867a715aaf28798aea53a7e0ad7b5788b
yuan miao <miao.yuan@ioart.com>
2015-07-27 22:41:54 +0800

    定时初始化

--- app/code/local/Yli/Recommend/Model/Related.php
+++ app/code/local/Yli/Recommend/Model/Related.php
@@ -5,6 +5,8 @@ class Yli_Recommend_Model_Related
 	{
 		//Who Bought This Also Bought 
 		$this->initBab();
+		//Who Viewed This Also Viewed
+		$this->initVav();
 	}
 	
 	private function initBab()
@@ -18,10 +20,33 @@ class Yli_Recommend_Model_Related
 		{
 			$customers = $redis->sMembers('product_order'.$product_id);
 			foreach ($customers as $customer_id){
-				$product_ids = $redis->sMembers('customer_order'.$product_id);
-				
+				$product_ids = $redis->sMembers('customer_order'.$customer_id);
+				foreach ($product_ids as $pid){
+				    $redis->zIncrby('product_bab'.$product_id,1,$pid);
+				}
 			}
 		}
 		
 	}
+	
+	private function initVav()
+	{
+	    $redis = Mage::helper('redis')->init(3);
+	    $products = Mage::getModel('catalog/product')->getCollection()
+        	    ->addAttributeToFilter('status',1)
+        	    ->getAllIds();
+	    
+	    foreach ($products as $product_id)
+	    {
+	        $customers = $redis->sMembers('product_view'.$product_id);
+	        foreach ($customers as $customer_id){
+	            $product_ids = $redis->sMembers('customer_view'.$customer_id);
+	            foreach ($product_ids as $pid){
+	                $redis->zIncrby('product_vav'.$product_id,1,$pid);
+	            }
+	        }
+	    }
+	}
+	
+	
 }
